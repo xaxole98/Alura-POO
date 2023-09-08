32f9053364beb8500a576234eddc2de9e90895d8
XaxoCode <xaxo55le@gmail.com>
2023-09-08 15:29:08 -0500

    Using Packages

--- demo/src/main/java/com/abdcbank/Cliente.java
+++ demo/src/main/java/com/abdcbank/Cliente.java
@@ -29,6 +29,4 @@ public class Cliente {
         this.telefono = telefono;
     }
 
-        
-
 }
--- demo/src/main/java/com/abdcbank/TestCuentas.java
+++ demo/src/main/java/com/abdcbank/TestCuentas.java
@@ -3,8 +3,8 @@ package com.abdcbank;
 public class TestCuentas {
 
     public static void main(String[] args) {
-        Cuenta cuenta1 = new Cuenta(111);
-        Cuenta cuenta2 = new Cuenta(888);
+        new Cuenta(111);
+        new Cuenta(888);
         System.out.println(Cuenta.getTotal());
 
     }
